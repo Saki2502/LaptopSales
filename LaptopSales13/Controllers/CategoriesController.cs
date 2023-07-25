1a621c80efd1f2c0ad05f1adec7529f6a5808fb1
tuyetnhi <tuyetnhi1610@gmail.com>
2023-07-26 05:05:01 +0700

    create new project

---
+++ LaptopSales13/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LaptopSales13.Models;
+
+namespace LaptopSales13.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        PasokonEntities db = new PasokonEntities();
+
+        // GET: Categories
+        public ActionResult Index()
+        {         
+            return View();
+        }
+
+        // Trả về View Category
+        public PartialViewResult CategoryPartial()
+        {
+            var cateList = db.Categories.OrderBy(x => x.CategoryName).ToList();
+            return PartialView(cateList);
+        }
+    }
+}
