504dfdf2dd5846d4e4948fa549a0042033b3451b
tuyetnhi <tuyetnhi1610@gmail.com>
2023-08-15 15:50:43 +0700

    update

---
+++ LaptopSales13/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaptopSales13.Controllers
+{
+    public class CategoryController : Controller
+    {
+        PasokonEntities db = new PasokonEntities();
+
+        // GET: Categories
+        public ActionResult Index()
+        {         
+            return View();
+        }
+
+        // Trả về View Category
+        public PartialViewResult CategoryPartial()
+        {
+            var cateList = db.Categories.OrderBy(x => x.CategoryName).ToList();
+            return PartialView(cateList);
+        }
+    }
+}
