ff5647b10640b54f7ec6d5b951b7a097560d8514
Nguyễn Ngọc Thiên Ân <nguyenngocthienan138@gmail.com>
2023-09-14 21:20:26 +0700

    admin

--- LaptopSales13/Areas/Admin/Controllers/HomeController.cs
+++ LaptopSales13/Areas/Admin/Controllers/HomeController.cs
@@ -5,15 +5,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Windows.Forms;
 
 namespace LaptopSales13.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
 
+        private PasokonEntities1 db = new PasokonEntities1();
+
         // GET: Admin/Home
         public ActionResult Index()
         {
+            int userCount = db.Accounts.Count(); // Số user trong cơ sở dữ liệu
+            int orderCount = db.Orders.Count(); // Số đơn hàng trong cơ sở dữ liệu
+
+            ViewBag.UserCount = userCount;
+            ViewBag.OrderCount = orderCount;
+
             return View();
         }
     }
